8c9aade09a335d326b0add7afac6f5efee92cc65
choseungyoun <seungyoun-cho@joseung-yun-ui-MacBookAir.local>
2024-01-31 14:28:37 +0900

    [test] 프로그램 test

---
+++ src/main/java/com/ohgiraffers/test/TestApplication.java
@@ -0,0 +1,25 @@
+package com.ohgiraffers.test;
+
+public class TestApplication {
+    //수업목표
+    //여기는 주석입니다.
+    /*  여기는
+    * 여러 줄
+    * 주석입니다람쥐
+    * */
+
+    /*
+    * ctrl - space : 자동완성 기능
+    * ctrl - d
+    * */
+
+    // 복사예시
+    // 복사예시
+    // 복사예시
+
+
+    public static void main(String[] args) {
+        System.out.println("저는 개발자입니다.\n 사실 바보입니다."); //소괄호안에있는것을 화면상으로 출력
+
+    }
+}
